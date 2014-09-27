97ca0b64bba27b056b02231fa9f6da3cdc05fb2d
Monique Pinto <niqueeta@gmail.com>
2014-09-27 16:59:25 +0100

    Add prev/next jquery functionality

--- source/javascripts/all.js.coffee
+++ source/javascripts/all.js.coffee
@@ -1,4 +1,6 @@
 //= require "modernizr.custom"
+//= require "jquery"
 //= require "classie"
 //= require "demo1"
 //= require "snap.svg-min"
+//= require "prevnext"
---
+++ source/javascripts/prevnext.js
@@ -0,0 +1,27 @@
+var pageList = ["index.html", "01.html", "02.html", "03.html"];
+var url = window.location.pathname;
+
+document.getElementById('next').onclick = function(){
+
+	var page = url.substring(url.lastIndexOf('/')+1);
+	var currentPosition = pageList.indexOf(page);
+
+    if(currentPosition==-1) return;
+
+    if(currentPosition<pageList.length-1) {
+    	window.location = pageList[++currentPosition];
+    } else {
+    	window.location = pageList[0];
+    }
+};
+
+document.getElementById('previous').onclick = function(){
+
+	var page = url.substring(url.lastIndexOf('/')+1);
+	var currentPosition = pageList.indexOf(page);
+
+    if(currentPosition==-1) return;
+
+    window.location = pageList[--currentPosition]; 
+};
+
